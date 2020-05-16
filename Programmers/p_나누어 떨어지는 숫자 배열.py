3b5e56bc282a0be64b4eca27e0c58121595b5d2d
yezyvibe <60089843+yezyvibe@users.noreply.github.com>
2020-05-16 18:46:29 +0900

    0516

---
+++ Programmers/p_나누어 떨어지는 숫자 배열.py
@@ -0,0 +1,11 @@
+def solution(arr, divisor):
+    answer = []
+    for num in arr:
+        if num % divisor == 0:
+            answer.append(num)
+
+    if len(answer) == 0:
+        answer = [-1]
+    else:
+        answer.sort()
+    return answer
