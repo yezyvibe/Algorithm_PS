3f771f346a9f37dfd5e5cfcd892977a65d7fc00b
yezyvibe <60089843+yezyvibe@users.noreply.github.com>
2020-08-22 23:37:21 +0900

    0818

--- BAEKJOON/b_11053_가장 긴 증가하는 부분 수열.py
+++ BAEKJOON/b_11053_가장 긴 증가하는 부분 수열.py
@@ -10,11 +10,3 @@ for i in range(1, n):
             cnt[i] = max(cnt[i], cnt[j] + 1)
 
 print(max(cnt))
-
-# 간단한 풀이
-# N = int(input())
-# nums = list(map(int, input().strip().split()))
-# dp = [0 for _ in range(1001)]
-# for i in range(N):
-#     dp[nums[i]] = max(dp[:nums[i]]) + 1
-# print(max(dp))
