74ceb72c3afd6d32394d9b882f6bca24c217d083
조예지 <yeji0501@gmail.com>
2020-08-25 23:21:23 +0900

    0825

--- Programmers/p_튜플.py
+++ Programmers/p_튜플.py
@@ -2,7 +2,17 @@ def solution(s):
     answer = []
     a = s.split(',{')
     a.sort(key = len)
-
-
-
-
+    tmp = ''
+    for i in a:
+        for k in i:
+            if k == ',':
+                if int(tmp) not in answer:
+                    answer.append(int(tmp))
+                tmp = ''
+            elif k !='{' and k != '}':
+                tmp += k
+        else:
+            if int(tmp) not in answer:
+                answer.append(int(tmp))
+            tmp = ''
+    return answer
