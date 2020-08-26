4e76018b8fcba5a34ae6f4b9756806bb8cdb6c15
조예지 <yeji0501@gmail.com>
2020-08-26 19:44:47 +0900

    0826

--- Programmers/p_프린터.py
+++ Programmers/p_프린터.py
@@ -1,24 +1,14 @@
 from collections import deque
 def solution(priorities, location):
-    priorities = deque(priorities)
-    l = len(priorities)
-    result = [0]*l
-    idx = 0
-    cnt = 1
-    while priorities:
-        a = priorities.popleft()
-        copy_lst = priorities.copy()
-        for b in copy_lst:
-            if a < b:
-                priorities.append(a)
-                idx += 1
-                break
+    answer = 0
+    array = deque([(v, i) for i, v in enumerate(priorities)])
+
+    while len(array):
+        item = array.popleft()
+        if array and max(array)[0] > item[0]:
+            array.append(item)
         else:
-            result[idx] = cnt
-            cnt += 1
-
-            if idx+1 == l:
-                idx = 0
-            else:
-                idx += 1
-
+            answer += 1
+            if item[1] == location:
+                break
+    return answer
