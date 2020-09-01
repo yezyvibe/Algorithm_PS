56592f5a36b1e82ee564ddbd6f31dd9f7619cf6b
조예지 <yeji0501@gmail.com>
2020-09-01 23:21:21 +0900

    0901

--- Programmers/p_체육복.py
+++ Programmers/p_체육복.py
@@ -9,16 +9,16 @@ def solution(n, lost, reserve):
             students[i] = 1
             tmp.append(i)
 
-    for i in tmp:
+    for i in tmp: # 여분 가지고 있는 학생이 체육복을 잃어 버린 경우
         lost.remove(i)
         reserve.remove(i)
 
     for i in reserve:
-        if i - 1 == 0:
+        if i - 1 == 0: # 인덱스 1인 경우에는 2번째만 체크
             if students[i + 1] == 0:
                 students[i + 1] = 1
             continue
-        if i + 1 == n + 1:
+        if i + 1 == n + 1: # 인덱스 n인 경우(끝값) n-1번째만 체크
             if students[i - 1] == 0:
                 students[i - 1] = 1
             continue
@@ -28,5 +28,5 @@ def solution(n, lost, reserve):
         if students[i + 1] == 0:
             students[i + 1] = 1
             continue
-            
+
     return sum(students)
