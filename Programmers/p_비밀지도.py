b2328e13846e093d99646a628600116e8e23d6ed
조예지 <yeji0501@gmail.com>
2020-09-09 23:45:13 +0900

    0909

---
+++ Programmers/p_비밀지도.py
@@ -0,0 +1,7 @@
+def solution(n, arr1, arr2):
+    answer = []
+    for i in range(n):
+        bin_str = bin(arr1[i] | arr2[i])[2:]
+        answer.append(('0' * (n - len(bin_str)) + bin_str).replace('0', ' ').replace('1', '#'))
+
+    return answer
