e96ae5cc4f160101b4e5806707035df17cbaf0be
조예지 <yeji0501@gmail.com>
2020-09-10 23:34:10 +0900

    0910

---
+++ Programmers/p_다트게임.py
@@ -0,0 +1,51 @@
+def solution(dartResult):
+    answer = 0
+    res = {}
+    idx = -1
+    check = 1
+    for i in dartResult:
+        if i.isdigit():
+            if check == 1:
+                idx += 1
+                res[idx] = ''
+                res[idx] += i
+            elif check == 0:
+                res[idx] = '_'
+            check = 0
+        else:
+            res[idx] += i
+            check = 1
+
+    for key, value in res.items():
+        tmp = 0
+        for i in value:
+            # 점수
+            if i.isdigit():
+                tmp = int(i)
+            # 10 처리
+            elif i == '_':
+                tmp = 10
+            # 보너스
+            elif i == 'S':
+                tmp = tmp ** 1
+            elif i == 'D':
+                tmp = tmp ** 2
+            elif i == 'T':
+                tmp = tmp ** 3
+
+            # 옵션
+            if i == '*':
+                tmp = tmp * 2
+                res[key] = tmp
+                if key != 0:
+                    res[key - 1] = res[key - 1] * 2
+                    break
+                break
+            elif i == '#':
+                res[key] = -tmp
+                break
+            res[key] = tmp
+
+    for i in res.values():
+        answer += i
+    return answer
