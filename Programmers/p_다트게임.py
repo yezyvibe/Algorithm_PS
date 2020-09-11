cfdbd714a316c9e3283b16d5e0072b2f5aa28bb9
조예지 <yeji0501@gmail.com>
2020-09-11 23:02:31 +0900

    0911

--- Programmers/p_다트게임.py
+++ Programmers/p_다트게임.py
@@ -3,13 +3,15 @@ def solution(dartResult):
     res = {}
     idx = -1
     check = 1
+
+    # 기회별 점수 -> 딕셔너리에 구분해 넣기
     for i in dartResult:
         if i.isdigit():
             if check == 1:
                 idx += 1
                 res[idx] = ''
                 res[idx] += i
-            elif check == 0:
+            elif check == 0: # 10처리
                 res[idx] = '_'
             check = 0
         else:
@@ -25,6 +27,7 @@ def solution(dartResult):
             # 10 처리
             elif i == '_':
                 tmp = 10
+
             # 보너스
             elif i == 'S':
                 tmp = tmp ** 1
@@ -45,7 +48,7 @@ def solution(dartResult):
                 res[key] = -tmp
                 break
             res[key] = tmp
-
+    # 합산
     for i in res.values():
         answer += i
     return answer
