1de491f1b622b59e2c2b96873ab2a620d11680f7
조예지 <yeji0501@gmail.com>
2020-10-05 00:23:14 +0900

    1005

---
+++ Programmers/p_예상 대진표.py
@@ -0,0 +1,15 @@
+def solution(n, a, b):
+    answer = 0
+
+    while a != b:
+        answer += 1
+        r_a, r_b = round(a / 2), round(b / 2)
+        if r_a < (a / 2):
+            a = r_a + 1
+        else:
+            a = r_a
+        if r_b < (b / 2):
+            b = r_b + 1
+        else:
+            b = r_b
+    return answer
