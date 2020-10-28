ea6d7d46b43bcd54bd064499166896f44a7acaf8
조예지 <yeji0501@gmail.com>
2020-10-28 10:33:15 +0900

    1028

--- BAEKJOON/b_14502_연구소.py
+++ BAEKJOON/b_14502_연구소.py
@@ -1,35 +1,27 @@
 import sys
 from itertools import combinations as combi
 from copy import deepcopy
+from collections import deque
 
-# dfs 최대!!
+# 점점 퍼져 나가는 건 bfs
 dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
 input = sys.stdin.readline
 
 
-def dfs(virus):
-    s = []
+def bfs(virus):
+    q = deque()
     for v in virus:
-        s.append(v)
-    # print(s,'시작')
-    while s:
-        x, y = s.pop()
-        print(x,y, 'xy')
+        q.append(v)
+
+    while q:
+        x, y = q.popleft()
         for k in range(4):
             nx, ny = x + dx[k], y + dy[k]
             if 0 <= nx < n and 0 <= ny < m and chk[nx][ny] == 0:
-                print(nx,ny,'11122222222222222222222')
                 chk[nx][ny] = 2
-                s.append((nx, ny))
-    # cnt = 0
-    # for i in range(n):
-    #     for j in range(m):
-    #         if chk[i][j] == 0:
-    #             cnt += 1
-    # return cnt
-    for c in chk:
-        print(c)
-    return
+                q.append((nx, ny))
+    return sum(chk, []).count(0)
+
 
 n, m = map(int, input().split(' '))
 bg = [list(map(int, input().rstrip().split(' '))) for _ in range(n)]
@@ -39,19 +31,18 @@ for i in range(n):
     for j in range(m):
         if bg[i][j] == 2:
             virus.append((i, j))
-            for k in range(4):
-                nx, ny = i + dx[k], j + dy[k]
-                if 0 <= nx < n and 0 <= ny < m and bg[nx][ny] == 0:
-                    candidates.append((nx, ny))
+        elif bg[i][j] == 0:
+            candidates.append((i, j))
+# print(virus)
+# print(candidates)
 
-print(candidates)
-print(virus, 'virus')
+max_v = -1
 for c in combi(candidates, 3):
-    print(c, 'combi')
     chk = deepcopy(bg)
-    for i, j in c:
-        chk[i][j] = 1
-    else:
-        print(dfs(virus))
-        # for c in chk:
-        #     print(c)
+    for x, y in c:
+        chk[x][y] = 1
+    tmp = bfs(virus)
+    if tmp > max_v:
+        max_v = tmp
+
+print(max_v)
