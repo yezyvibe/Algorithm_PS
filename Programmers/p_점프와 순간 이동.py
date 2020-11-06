07379abd9fef5442e680bda3d598f88d9bd5a204
조예지 <yeji0501@gmail.com>
2020-11-06 14:30:10 +0900

    1106

---
+++ Programmers/p_점프와 순간 이동.py
@@ -0,0 +1,12 @@
+def solution(n):
+    cnt = 0
+    while n != 0:
+        if n % 2 == 0:
+            n /= 2
+        else:
+            n -= 1
+            cnt += 1
+    print(cnt)
+
+
+solution(5000)
