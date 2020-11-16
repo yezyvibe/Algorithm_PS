8504aff15f5727f8b4c4a8864dca17fefa5866b5
조예지 <yeji0501@gmail.com>
2020-11-16 17:56:32 +0900

    1116

---
+++ Algorithm Study/b_9251_LCS.py
@@ -0,0 +1,13 @@
+s1 = input()
+s2 = input()
+
+matrix = [[0] * (len(s2) + 1) for _ in range(len(s1) + 1)]
+
+for i in range(1, len(s1) + 1):
+    for j in range(1, len(s2) + 1):
+        if s1[i - 1] == s2[j - 1]:
+            matrix[i][j] = matrix[i - 1][j - 1] + 1
+        else:
+            matrix[i][j] = max(matrix[i - 1][j], matrix[i][j - 1])
+
+print(matrix[-1][-1])
