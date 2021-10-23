5512548e0c5ee8f954695a5f70cb52336d0c5422
yezyvibe <yeji0501@gmail.com>
2021-10-23 22:19:31 +0900

    p_숫자의 표현.py

---
+++ p_숫자의 표현.py
@@ -0,0 +1,15 @@
+def solution(n):
+    answer = 0
+    for i in range(1,n+1):
+        num = 0
+        for j in range(i, n+1):
+            num += j
+            if num == n:
+                answer += 1
+                num = i
+            elif num > n:
+                break
+    return answer
+
+
+print(solution(15))
