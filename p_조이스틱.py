c9c60c726fe2292a1c2b703f5559bce4357c5609
yezyvibe <yeji0501@gmail.com>
2021-10-26 09:46:44 +0900

    p_조이스틱.py

---
+++ p_조이스틱.py
@@ -0,0 +1,24 @@
+def solution(name):
+    # 상하이동
+    change = [min(ord(i)-ord('A'), ord('Z')-ord(i) + 1) for i in name]
+    idx = 0
+    answer = 0
+
+    while True:
+        answer += change[idx]
+        change[idx] = 0
+        if sum(change) == 0:
+            return answer
+        
+        # 좌우이동
+        left, right = 1, 1
+        while change[idx - left] == 0:
+            left += 1
+        while change[idx + right] == 0:
+            right += 1
+        answer += left if left < right else right
+        idx += -left if left < right else right
+    
+    return answer
+
+print(solution("JEROEN"))
