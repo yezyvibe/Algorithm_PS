ae41a61bc92a09878327d600645e5f2228575818
yezyvibe <yeji0501@gmail.com>
2021-11-08 21:10:10 +0900

    p_9주차 전력망을 둘로 나누기.py

---
+++ p_9주차 전력망을 둘로 나누기.py
@@ -0,0 +1,35 @@
+def connect(lst, i, adj):
+    stack = []
+    for i in lst:
+        stack.append(i)
+    visit = [0]*len(adj)
+    visit[i-1] = 1
+    while stack:
+        a = stack.pop()
+        for i in adj[a-1]:
+            if not visit[i-1]:
+                stack.append(i)
+                visit[i-1] = 1
+    
+    return visit
+
+def solution(n, wires):
+    adj = [[] for _ in range(n)]
+    
+    for a, b in wires:
+        adj[a-1].append(b)
+        adj[b-1].append(a)
+    print(adj)
+    min_v = 100
+    for i in range(n):
+        for j in range(len(adj[i])):
+            tmp = adj[i][j]
+            adj[i].remove(adj[i][j])
+            if adj[i]:
+                res = connect(adj[i], i, adj)
+                min_v = min(min_v, abs(res.count(0) - res.count(1)))
+            adj[i] = [tmp] + adj[i]
+    return min_v
+n = 9   
+wires = [[1,3],[2,3],[3,4],[4,5],[4,6],[4,7],[7,8],[7,9]]
+print(solution(n, wires))
