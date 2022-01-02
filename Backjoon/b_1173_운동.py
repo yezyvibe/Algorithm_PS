63830861a3bceb76e09143bc5b0476fec30d2320
yezyvibe <yeji0501@gmail.com>
2022-01-02 22:46:08 +0900

    b_1173_운동.py

---
+++ Backjoon/b_1173_운동.py
@@ -0,0 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+N, m, M, T, R = map(int, input().split())
+pulse = m
+time = 0
+exercise = 0
+
+while exercise < N:
+    if m + T > M:
+        time = -1
+        break
+    time += 1
+    if pulse + T <= M:
+        pulse += T
+        exercise += 1
+    else:
+        if pulse - R < m:
+            pulse = m
+        else:
+            pulse -= R
+print(time)
+    
