f6939ac91cec2093305f44535fe580c5e32d3098
yezyvibe <yeji0501@gmail.com>
2022-03-20 10:00:10 +0000

    b_17413_단어뒤집기2.py

---
+++ Backjoon/b_17413_단어뒤집기2.py
@@ -0,0 +1,22 @@
+import sys
+
+input = sys.stdin.readline
+word = list(input().rstrip())
+i = 0
+start = 0
+while i < len(word):
+    if word[i] == '<':   # 태그일 때는 그냥 두기
+        i += 1
+        while word[i] != '>':
+            i += 1
+        i += 1
+    elif word[i].isalnum():
+        start = i
+        while i < len(word) and word[i].isalnum():
+            i += 1
+        tmp = word[start:i]
+        tmp.reverse()
+        word[start:i] = tmp
+    else:
+        i += 1
+print(''.join(word))
