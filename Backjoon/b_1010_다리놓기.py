faf6de2770ad5c7a07a0d642340fd8a69a98f99e
yezyvibe <yeji0501@gmail.com>
2022-10-14 01:20:52 +0900

    b_1010_다리놓기.py

---
+++ Backjoon/b_1010_다리놓기.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+for i in range(int(input())):
+    n, m = map(int, input().split())
+    dp = [[0]*(m+1) for _ in range(n+1)]
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            if i == 1:
+                dp[i][j] = j
+                continue
+            if i == j:
+                dp[i][j] = 1
+            elif j > i:
+                dp[i][j] = dp[i-1][j-1] + dp[i][j-1]
+    print(dp[n][m])
